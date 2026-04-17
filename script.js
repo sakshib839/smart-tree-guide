5ff61fd468cc1f02777dfb00332a2eeab8f85d84
sakshi bodke <136315477+sakshib839@users.noreply.github.com>
2026-04-17 23:48:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function goToEnglish() {
+    window.location.href = "english.html";
+}
+
+function goToMarathi() {
+    window.location.href = "marathi.html";
+}
+
+function goBack() {
+    window.location.href = "index.html";
+}
